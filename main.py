63832d102e2e7a4350dfa5aa8c6fe91fbad723e9
MaksssM <maksymmilinchyk@gmail.com>
2022-12-12 21:43:56 +0200

    1

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import discord
 import json
 from discord.ext import commands
+
+intents = discord.Intents.all()
+intents.members = True
+
+file = open('config.json', 'r')
+config = json.load(file)
+
+bot = commands.Bot(config['prefix'], intents = intents)
+@bot.command(name='ping')
+async def ping(ctx):
+    await ctx.send(f'pong!')
+
+
+bot.run(config['token'])

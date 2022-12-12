65e60188a8edf3dfef419c1e58835e66139052d0
MaksssM <maksymmilinchyk@gmail.com>
2022-12-12 23:48:36 +0200

    1

--- main.py
+++ main.py
@@ -8,10 +8,16 @@ intents.members = True
 file = open('config.json', 'r')
 config = json.load(file)
 
-bot = commands.Bot(config['prefix'], intents = intents)
+bot = commands.Bot(config['prefix'], intents=intents)
+
+
 @bot.command(name='ping')
 async def ping(ctx):
     await ctx.send(f'pong!')
+
+@bot.command(name='clear')
+async def clear(ctx, amount = 1):
+    await ctx.channel.purge(limit = amount)
 
 
 bot.run(config['token'])

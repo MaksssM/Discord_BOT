b1e55b5c79e65599b80e2ce065cfdfe86e91b04c
MaksM <maksimmilinchyk@gmail.com>
2023-01-31 00:03:35 +0200

    po novoy

--- main.py
+++ main.py
@@ -1,23 +1,75 @@
 import discord
 import json
+import sqlite3
 from discord.ext import commands
 
-intents = discord.Intents.all()
-intents.members = True
+client = commands.Bot(command_prefix = config['prefix'])
 
-file = open('config.json', 'r')
-config = json.load(file)
+connect = sqlite3.connect('zalupa')
+curs = connect.cursor()
 
-bot = commands.Bot(config['prefix'], intents=intents)
+@client.event
+async def on_ready():
+    curs.execute("""Create Table if NOT exists users (
+        name TEXT,
+        id INT,
+        cash BIGINT
+    )""")
+    connect.commit()    
+
+    if curs.execute(f"Select id from users where id = {member.id}").fetchone() is None:
+        curs.execute(f"INSERT Into users Values ('{member}', {member.id}, {0})")
+    else:
+        pass
+
+@client.event 
+async def on_member_join(member):
+    if curs.execute(f"Select id from users where id = {member.id}").fetchone() is None:
+        curs.execute(f"INSERT Into users Values ('{member}', {member.id}, {0})")
+        connect.commit()
+    else:
+        pass
+
+@client.command(aliases = ['balance'])
+async def _balance(ctx, member:discord.Member = None):
+    if member is None:
+        await ctx.send(embed = discord.Embed(
+            description = f"""Баланс гандона**{ctx.author}** **{curs.execute("Select cash From users where id = {}".format(ctx.author.id)).fetchone()[0]} **"""
+        ))
+    else:
+        await ctx.send(embed = discord.Embed(
+            description = f"""Твой баланс**{member}** **{curs.execute("Select cash From users where id = {}".format(member.id)).fetchone()[0]} **"""
 
 
-@bot.command(name='ping')
-async def ping(ctx):
-    await ctx.send(f'pong!')
 
-@bot.command(name='clear')
-async def clear(ctx, amount = 1):
-    await ctx.channel.purge(limit = amount)
+@client.command(aliases = ['award'])
+async def _award(ctx, member:discord.Member = None, amount: int = None)
+    if member is None:
+        await ctx.send(f"**{ctx.author}, укажите гандона, которому выдать баланс")
+    else:
+        if amount is None:
+            await ctx.send(f"**{ctx.author}, укажите сумму, которую начисляешь")
+            elif amount < 1:
+                await ctx.send(f"**{ctx.author}, укажи больше 1**")
+            else:
+                curs.execute("Update users Set cash = cash + {} Where id = {}".format(amount, member.id))
+                connect.commit()
 
 
-bot.run(config['token'])
+
+              
+@client.command(aliases = ['take'])
+async def _take(ctx, member:discord.Member = None, amount = None)
+    if member is None:
+        await ctx.send(f"**{ctx.author}, укажите гандона, которому выдать баланс")
+    else:
+        if amount is None:
+            await ctx.send(f"**{ctx.author}, укажите сумму, которую забираешь")
+            elif amount == 'all':
+               curs.execute("Update users Set cash = {} Where id = {}".format(0, member.id))
+                connect.commit() 
+            elif int(amount) < 1:
+                await ctx.send(f"**{ctx.author}, укажи больше 1**")
+            else:
+                curs.execute("Update users Set cash = cash - {} Where id = {}".format(int(amount, member.id))
+                connect.commit()
